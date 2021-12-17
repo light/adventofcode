2eebff8071f8a2cbab4446196c7321f7d940b24b
vic <vic@bidouille.org>
2021-12-17 15:28:22 +0100

    Seventeeth day, less useless calculations

--- 17/puzzle.py
+++ 17/puzzle.py
@@ -7,25 +7,6 @@ l = next(inputs())
 l.match('target area: x=(.+)\.\.(.+), y=(.+)\.\.(.+)')
 xmin = l.int(1); xmax = l.int(2); ymin = l.int(3); ymax = l.int(4)
 
-# y_t+1 = y_t + vy_t
-# y_0 = 0
-# vy_t+1 = vy_t - 1
-# => vy_t = vy_0 - t
-# => y_1 = vy_0
-#    y_2 = vy_0 + vy_0 - 1
-#    y_3 = 3*vy_0 - 2
-# => y_t = t*vy_0 - t*(t-1)/2
-def y(vy_0, t): return t*vy_0 - t*(t-1)//2
-
-# x_t+1 = x_t + vx_t
-# x_0 = 0
-# vx_t+1 = vx_t + (vx_t > 0 ? 1 : 0) # Assume positive vx
-# => vx_t = t < vx_0 ? vx_0 - t : 0
-# => x_t = t*vx_0 - t(t-1)/2 | t < vx_0
-#    x_t = vx_0^2 - vx_0(vx_0-1)/2 | t >= vx_0
-#    x_t = vx_0(vx_0+1)/2 | t >= vx_0
-def x(vx_0, t): return t*vx_0 - t*(t-1)//2 if t < vx_0 else vx_0*(vx_0+1)//2
-
 # Min vx_0 to never undershoot :
 # x_t_inf >= xmin
 #  => vx_0(vx_0+1)/2 >= xmin
@@ -56,21 +37,22 @@ vy_0_max = -ymin-1
 
 print(f"Search space: vx={vx_0_min}..{vx_0_max}, vy={vy_0_min}..{vy_0_max}")
 
-maxy = 0
+allmaxy = 0
 count = 0
 for vx_0 in range(vx_0_min, vx_0_max+1):
   for vy_0 in range(vy_0_min, vy_0_max+1):
-    t = 0
-    _x = _y = _maxy = 0
-    while _x <= xmax and _y >= ymin:
-      _x = x(vx_0, t)
-      _y = y(vy_0, t)
-      _maxy = max(_maxy, _y)
-      if _x <= xmax and _x >= xmin and _y >= ymin and _y <= ymax:
-        maxy = max(maxy, _maxy)
+    t = x = y = maxy = 0
+    vx = vx_0; vy = vy_0
+    while x <= xmax and y >= ymin:
+      x += vx; y += vy
+      vx = vx-1 if vx > 0 else 0
+      vy -= 1
+      maxy = max(maxy, y)
+      if x <= xmax and x >= xmin and y >= ymin and y <= ymax:
+        allmaxy = max(allmaxy, maxy)
         count += 1
         break
       t += 1
 
-print_res("Part one", maxy, 1)
+print_res("Part one", allmaxy, 1)
 print_res("Part two", count, 2)
