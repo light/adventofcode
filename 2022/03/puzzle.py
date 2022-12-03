2b2bf504fd1776c63c6a1f4fcea312a8d747e2d3
vic <vic@bidouille.org>
2022-12-03 16:45:00 +0100

    2022-03 Part two

--- 2022/03/puzzle.py
+++ 2022/03/puzzle.py
@@ -6,13 +6,23 @@ from board import Board, P
 def p(c):
   v = ord(c)
   return v-64+26 if v < 97 else v-96
-def common(a, b):
-  c = set(a).intersection(set(b))
+def common(*a):
+  c = set(a[0])
+  for b in a[1:]:
+    c = c.intersection(set(b))
   return c.pop()
 
-s = 0
+score1 = 0
+score2 = 0
+group = []
 for l in inputs():
   a, b = l[:int(len(l)/2)], l[int(len(l)/2):]
-  s += p(common(a, b))
+  score1 += p(common(a, b))
+  group.append(l)
+  if len(group) == 3:
+    c = common(common(group[0], group[1], group[2]))
+    score2 += p(c)
+    group = []
 
-print_res("Part one:", s, 1)
+print_res("Part one:", score1, 1)
+print_res("Part two:", score2, 2)
