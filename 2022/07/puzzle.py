8d2ac64800b4bbe7f27d2308cbc36b6033280a79
vic <vic@bidouille.org>
2022-12-07 12:31:49 +0100

    2022-07 Part one

---
+++ 2022/07/puzzle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env -S PYTHONPATH=../../util pypy3
+
+from util import *
+
+def empty_dir(parent): return { "dirs": {}, "size": 0, "parent": parent }
+root = empty_dir(None)
+cwd = None
+for l in inputs():
+  if l.match('\$ cd (.+)'):
+    dir = l.str(1)
+    if dir == "/":
+      cwd = root
+    elif dir == "..":
+      cwd = cwd["parent"]
+    else:
+      cwd = cwd["dirs"][dir]
+  elif l.match('\$ ls'):
+    pass
+  elif l.match('dir (.+)'):
+      cwd["dirs"][l.str(1)] = empty_dir(cwd)
+  elif l.match('(\d+) .+'):
+    d = cwd
+    while d is not None:
+      d["size"] += l.int(1)
+      d = d["parent"]
+
+def walk(cwd, fun, acc=0):
+  if cwd is None:
+    return acc
+  acc += fun(cwd)
+  for dir in cwd["dirs"].values():
+    acc += walk(dir, fun)
+  return acc
+
+size = walk(root, lambda dir: dir["size"] if dir["size"] <= 100000 else 0)
+print_res("Part one:", size, 1)
