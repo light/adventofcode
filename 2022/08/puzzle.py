f72d44a7ce91ba204c1f429c3a3be2ef1d1760bf
vic <vic@bidouille.org>
2022-12-10 22:25:33 +0100

    2022-08 Part two

--- 2022/08/puzzle.py
+++ 2022/08/puzzle.py
@@ -41,5 +41,30 @@ for y in range(trees.h):
     if h > v[0] or h > v[1] or h > v[2] or h > v[3]:
       n_visibles += 1
 
-
 print_res("Part one:", n_visibles, 1)
+
+max_scenic = 0
+for y in range(trees.h):
+  for x in range(trees.w):
+    h = trees.get(x, y)
+    a, b, c, d = 0, 0, 0, 0
+    for i in range(x):
+      a += 1
+      if trees.get(x-i-1, y) >= h:
+        break
+    for i in range(x+1, trees.w):
+      b += 1
+      if trees.get(i, y) >= h:
+        break
+    for j in range(y):
+      c += 1
+      if trees.get(x, y-j-1) >= h:
+        break
+    for j in range(y+1, trees.h):
+      d += 1
+      if trees.get(x, j) >= h:
+        break
+    scenic = a*b*c*d
+    max_scenic = max(max_scenic, a*b*c*d)
+
+print_res("Part two:", max_scenic, 2)
