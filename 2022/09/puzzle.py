a1a5faf0fafc66da2e97ff2ce205bdf3d9b4cbb6
vic <vic@bidouille.org>
2022-12-11 17:43:57 +0100

    2022-09 Part two

--- 2022/09/puzzle.py
+++ 2022/09/puzzle.py
@@ -4,20 +4,31 @@ from util import *
 from board import Board
 
 dirs = {"U": [0,1], "D": [0,-1], "R": [1,0], "L": [-1,0]}
-rope = [0,0,0,0] # head xy, tail xy
-map = Board('.')
-map.set(rope[2], rope[3], '#')
+steps = [l for l in inputs()]
+
 def sgn(a, b): return 1 if a > b else -1 if a < b else 0
-for l in inputs():
-  l.match('(.) (\d+)')
-  d = dirs[l.str(1)]
-  for i in range(l.int(2)):
-    rope[0] += d[0]; rope[1] += d[1]
-    if abs(rope[0] - rope[2]) >= 2 or abs(rope[1] - rope[3]) >= 2:
-      rope[2] += sgn(rope[0], rope[2])
-      rope[3] += sgn(rope[1], rope[3])
-    map.set(rope[2], rope[3], '#')
+def simulate_rope(steps, knots):
+  rope = [[0,0] for i in range(knots)] # knots xy
+  map = Board('.')
+  map.set(0, 0, '#')
+  for s in steps:
+    s.match('(.) (\d+)')
+    d = dirs[s.str(1)]
+    for i in range(s.int(2)):
+      knot = rope[0]
+      knot[0] += d[0]; knot[1] += d[1]
+      for k in range(1, len(rope)):
+        knotA, knotB = rope[k-1], rope[k]
+        if abs(knotA[0] - knotB[0]) >= 2 or abs(knotA[1] - knotB[1]) >= 2:
+          knotB[0] += sgn(knotA[0], knotB[0])
+          knotB[1] += sgn(knotA[1], knotB[1])
+      tail = rope[-1]
+      map.set(tail[0], tail[1], '#')
+  return map
 
-visited = map.visit(lambda count, x, y, v: count+1 if v == "#" else count, 0)
+def visited(steps, knots):
+  map = simulate_rope(steps, knots)
+  return map.visit(lambda count, x, y, v: count+1 if v == "#" else count, 0)
 
-print_res("Part one:", visited, 1)
+print_res("Part one:", visited(steps, 2), 1)
+print_res("Part one:", visited(steps, 10), 2)
