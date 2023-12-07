3720f062c9c4a8ceacce937aa5f7e8ccdd225d97
vic <vic@bidouille.org>
2023-12-07 13:54:33 +0100

    2023-01 Part Two

--- 2023/01/puzzle.py
+++ 2023/01/puzzle.py
@@ -3,9 +3,34 @@
 from util import *
 import re
 
+numbers = {
+  "one": "1",
+  "two": "2",
+  "three": "3",
+  "four": "4",
+  "five": "5",
+  "six": "6",
+  "seven": "7",
+  "eight": "8",
+  "nine": "9",
+}
+
+def first_digit(l, reverse=False):
+  if reverse:
+    l = l[::-1]
+  for i in range(len(l)):
+    s = l[i:]
+    if(s[0].isdigit()):
+      return s[0]
+    for k, v in numbers.items():
+      if reverse:
+        k = k[::-1]
+      if s.startswith(k):
+        return v
+
 s = 0
 for l in inputs():
-  digits = re.findall(r'\d', l)
-  s += int(digits[0]+digits[-1])
+  s += int(first_digit(l)+first_digit(l, True))
 
 print_res("Part one:", s, 1)
+print_res("Part two:", s, 2)
