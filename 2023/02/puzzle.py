8d414e9db503fa14a4b6b9d79673ff0156229a86
vic <vic@bidouille.org>
2023-12-07 16:15:00 +0100

    2023-02 Part Two

--- 2023/02/puzzle.py
+++ 2023/02/puzzle.py
@@ -24,13 +24,26 @@ def possible(plays):
       return False
   return True
 
-score = 0
+def maxCubes(plays):
+  maxRGB = [0, 0, 0]
+  for play in plays.split("; "):
+    rgb = parse_play(play)
+    maxRGB[0] = max(maxRGB[0], rgb[0])
+    maxRGB[1] = max(maxRGB[1], rgb[1])
+    maxRGB[2] = max(maxRGB[2], rgb[2])
+  return maxRGB
+
+score1 = 0
+score2 = 0
 for l in inputs():
   l.match(r'Game (\d+): (.*)')
   gameId = l.int(1)
   plays = l.str(2)
   if possible(plays):
-    score += gameId
+    score1 += gameId
+  rgb = maxCubes(plays)
+  power = rgb[0]*rgb[1]*rgb[2]
+  score2 += power
 
-
-print_res("Part one:", score, 1)
+print_res("Part one:", score1, 1)
+print_res("Part two:", score2, 2)
