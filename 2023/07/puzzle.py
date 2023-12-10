5bcc9b7559262d8a25ceab4f52348170194291e9
vic <vic@bidouille.org>
2023-12-10 03:11:01 +0100

    2023-07 Part Two

--- 2023/07/puzzle.py
+++ 2023/07/puzzle.py
@@ -9,7 +9,8 @@ class Bid(NamedTuple):
   bid: int
   type: int
 
-cards = "AKQJT98765432"
+cards1 = "AKQJT98765432"
+cards2 = "AKQT98765432J"
 Five_of_a_kind = 1
 Four_of_a_kind = 2
 Full_house = 3
@@ -18,30 +19,60 @@ Two_pair = 5
 One_pair = 6
 High_card = 7
 
-def parse_bid(l):
-  a, b = l.split()
-  b = int(b)
-  c = list(set([(c, a.count(c)) for c in a]))
+def rules1(hand):
+  c = list(set([(c, hand.count(c)) for c in hand]))
   if len(c) == 1:
-    return Bid(a, b, Five_of_a_kind)
+    return Five_of_a_kind
   elif len(c) == 2:
     if c[0][1] == 4 or c[1][1] == 4:
-      return Bid(a, b, Four_of_a_kind)
+      return Four_of_a_kind
     else:
-      return Bid(a, b, Full_house)
+      return Full_house
   elif len(c) == 3:
     if c[0][1] == 3 or c[1][1] == 3 or c[2][1] == 3:
-      return Bid(a, b, Three_of_a_kind)
+      return Three_of_a_kind
     else:
-      return Bid(a, b, Two_pair)
+      return Two_pair
   elif len(c) == 4:
-    return Bid(a, b, One_pair)
+    return One_pair
   else:
-    return Bid(a, b, High_card)
+    return High_card
 
-bids = [parse_bid(l) for l in inputs()]
+def rules2(hand):
+  jokers = hand.count("J")
+  hand = hand.replace("J", "")
+  c = list(set([(c, hand.count(c)) for c in hand]))
+  c.sort(key=lambda x: x[1])
+  if jokers == 5:
+    c = [("A", 0)]
+  c[-1] = (c[-1][0], c[-1][1]+jokers)
 
-def cmp_bids(a, b):
+  if len(c) == 1:
+    return Five_of_a_kind
+  elif len(c) == 2:
+    if c[0][1] == 4 or c[1][1] == 4:
+      return Four_of_a_kind
+    else:
+      return Full_house
+  elif len(c) == 3:
+    if c[0][1] == 3 or c[1][1] == 3 or c[2][1] == 3:
+      return Three_of_a_kind
+    else:
+      return Two_pair
+  elif len(c) == 4:
+    return One_pair
+  else:
+    return High_card
+
+
+def parse_bid(l, rules):
+  a, b = l.split()
+  return Bid(a, int(b), rules(a))
+
+
+bids = [l for l in inputs()]
+
+def cmp_bids(a, b, cards):
   if a.type < b.type:
     return 1
   elif a.type > b.type:
@@ -57,11 +88,13 @@ def cmp_bids(a, b):
     return 0
 
 
-def score(bids):
-  bids = sorted(bids, key=cmp_to_key(cmp_bids))
+def score(bids, rules, cards):
+  bids = [parse_bid(bid, rules) for bid in bids]
+  bids = sorted(bids, key=cmp_to_key(lambda a, b: cmp_bids(a, b, cards)))
   score = 0
   for i in range(len(bids)):
     score += (i+1)*bids[i].bid
   return score
 
-print_res("Part one:", score(bids), 1)
+print_res("Part one:", score(bids, rules1, cards1), 1)
+print_res("Part one:", score(bids, rules2, cards2), 2)
