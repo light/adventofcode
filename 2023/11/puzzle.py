047a66987b785ed6ef265718716688780a3a84ce
vic <vic@bidouille.org>
2023-12-11 18:03:34 +0100

    2023-11 Part Two

--- 2023/11/puzzle.py
+++ 2023/11/puzzle.py
@@ -10,23 +10,24 @@ for y, l in enumerate(inputs()):
   for x, v in enumerate(l):
     universe.set(x, y, v)
 
-# Expand Universe
-for y in range(universe.h-1, -1, -1):
+empty_rows = []
+empty_cols = []
+for y in range(universe.h):
   all_empty = True
   for x in range(universe.w):
     if universe.get(x, y) != ".":
       all_empty = False
       break
   if all_empty:
-    universe.insertRow(y)
-for x in range(universe.w-1, -1, -1):
+    empty_rows.append(y)
+for x in range(universe.w):
   all_empty = True
   for y in range(universe.h):
     if universe.get(x, y) != ".":
       all_empty = False
       break
   if all_empty:
-    universe.insertCol(x)
+    empty_cols.append(x)
 
 galaxies = []
 for y in range(universe.h):
@@ -34,11 +35,28 @@ for y in range(universe.h):
     if universe.get(x, y) == "#":
       galaxies.append(universe.getP(x, y))
 
-score1 = 0
-for i, a in enumerate(galaxies[:-1]):
-  for b in galaxies[i+1:]:
-    score1 += abs(a.x-b.x)+abs(a.y-b.y)
+
+# Return index of val in sorted list
+def index_in_list(list, val):
+  for i, v in enumerate(list):
+    if val < v:
+      return i
+  return len(list)
+def distance(a, b, empty_factor):
+  num_empty_rows = index_in_list(empty_rows, a.y) - index_in_list(empty_rows, b.y)
+  num_empty_cols = index_in_list(empty_cols, a.x) - index_in_list(empty_cols, b.x)
+  return abs(a.x-b.x)+abs(a.y-b.y)+(abs(num_empty_rows)+abs(num_empty_cols))*(empty_factor-1)
+
+def sum_distances(galaxies, empty_factor):
+  dist = 0
+  for i, a in enumerate(galaxies[:-1]):
+    for b in galaxies[i+1:]:
+      dist += distance(a, b, empty_factor)
+  return dist
+
+score1 = sum_distances(galaxies, 2)
+score2 = sum_distances(galaxies, int(cmdline_arg(1)))
 
 
-
-print_res("Part one:", score1, 1)
+print_res("Part one:", score1, 2)
+print_res("Part two:", score2, 3)
