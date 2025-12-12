476e476b96e3416ca80caa7d4c3363fbcba0c381
vic <vic@bidouille.org>
2025-12-12 20:34:51 +0100

    2025-03 Part Two

--- 2025/03/puzzle.py
+++ 2025/03/puzzle.py
@@ -4,21 +4,26 @@ from util import *
 
 banks = [l for l in inputs()]
 
-score1 = 0
 
-for bank in banks:
-  i1 = None
-  for i in range(len(bank) - 2, -1, -1):
+def find_greatest(bank):
+  imax = None
+  for i in range(len(bank)):
     d = bank[i]
-    if i1 is None or d >= bank[i1]:
-      i1 = i
-  i2 = None
-  for i in range(i1+1, len(bank)):
-    d = bank[i]
-    if i2 is None or d > bank[i2]:
-      i2 = i
-  j = bank[i1]+bank[i2]
-  score1 += int(j)
+    if imax is None or d > bank[imax]:
+      imax = i
+  return imax
+
+def max_joltage(bank, n):
+  istart = 0
+  digits = []
+  for k in range(n):
+    i = find_greatest(bank[istart:len(bank)-(n-k)+1]) + istart
+    digits.append(bank[i])
+    istart = i+1
+  return int("".join(digits))
+
+score1 = sum(max_joltage(bank, 2) for bank in banks)
+score2 = sum(max_joltage(bank, 12) for bank in banks)
 
 print_res("Part one:", score1, 1)
-
+print_res("Part two:", score2, 2)
