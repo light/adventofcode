29108fd31cb1a91127e526ed74929e5d08c52e9e
vic <vic@bidouille.org>
2025-12-12 13:18:24 +0100

    2025-02 Part Two

--- 2025/02/puzzle.py
+++ 2025/02/puzzle.py
@@ -4,14 +4,26 @@ from util import *
 
 
 score1 = 0
+score2 = 0
 
 for r in next(inputs()).split(","):
   a, b = [int(i) for i in r.split("-")]
   for i in range(a, b + 1):
     id = str(i)
-    if len(id) % 2 == 0:
-      m = len(id) // 2
-      if id[:m] == id[m:]:
-        score1 += int(id)
+    l = len(id)
+    for d in range(2, l+1):
+      if l % d == 0:
+        m = l // d
+        equals = True
+        for k in range(1, d):
+          if id[:m] != id[m*k:m*(k+1)]:
+            equals = False
+            break
+        if equals:
+          if d == 2:
+            score1 += int(id)
+          score2 += int(id)
+          break
 
 print_res("Part one:", score1, 1)
+print_res("Part two:", score2, 2)
