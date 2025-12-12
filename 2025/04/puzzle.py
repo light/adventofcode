24333bff3fb5a29240d649be9710eb863b43c226
vic <vic@bidouille.org>
2025-12-12 21:20:49 +0100

    2025-04 Part Two

--- 2025/04/puzzle.py
+++ 2025/04/puzzle.py
@@ -8,13 +8,30 @@ for y, l in enumerate(inputs()):
   for x, v in enumerate(l):
     rolls.set(x, y, v)
 
-def accessible_roll(x, y):
+def is_accessible_roll(x, y):
   p = rolls.getP(x, y)
   if p.val != "@":
     return False
   n = sum(1 if p.val == '@' else 0 for p in rolls.neighbors8(p))
   return n < 4
 
-score1 = rolls.visit(lambda count, x, y, v: count+1 if accessible_roll(x, y) else count, 0)
+def remove_accessible_rolls(rolls):
+  accessible_rolls = rolls.visit(lambda acc, x, y, v: acc+[P(x,y)] if is_accessible_roll(x, y) else acc, [])
+  for p in accessible_rolls:
+    rolls.set(p.x, p.y, ".")
+  return len(accessible_rolls)
+
+def remove_all_accessible_rolls(rolls):
+  sum = 0
+  while True:
+    n = remove_accessible_rolls(rolls)
+    if n == 0:
+      break
+    sum += n
+  return sum
+
+score1 = remove_accessible_rolls(rolls.copy())
+score2 = remove_all_accessible_rolls(rolls)
 
 print_res("Part one:", score1, 1)
+print_res("Part two:", score2, 2)
